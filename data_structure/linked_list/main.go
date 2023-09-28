fee1df15e8d95e598642cc01e8882d713ab3ae21
robsonandradev <robsom.s.a@gmail.com>
2023-09-28 15:05:45 -0300

    Display back to front

--- data_structure/linked_list/main.go
+++ data_structure/linked_list/main.go
@@ -9,27 +9,33 @@ func main() {
   l.Append("3")
   l.Append("4")
   l.Display()
+  l.DisplayReversed()
 }
 
 type Node struct {
-  data string
-  next *Node
+  data  string
+  prior *Node
+  next  *Node
 }
 
 type LinkedList struct {
   head *Node
+  last *Node
 }
 
 func (l *LinkedList) Append(data string) {
   currentNode := l.head
   if l.head == nil {
-    l.head = &Node{data: data, next: nil}
+    l.head = &Node{data: data}
     return
   }
   for currentNode.next != nil {
     currentNode = currentNode.next
   }
-  currentNode.next = &Node{data: data, next: nil}
+  newNode := &Node{data: data, next: nil}
+  currentNode.next = newNode
+  newNode.prior = currentNode
+  l.last = newNode
 }
 
 func (l *LinkedList) Display() {
@@ -39,5 +45,15 @@ func (l *LinkedList) Display() {
     items = append(items, currentNode.data)
     currentNode = currentNode.next
   }
-  fmt.Print(items)
+  fmt.Println(items)
+}
+
+func (l *LinkedList) DisplayReversed() {
+  currentNode := l.last
+  items := []string{}
+  for currentNode != nil {
+    items = append(items, currentNode.data)
+    currentNode = currentNode.prior
+  }
+  fmt.Println(items)
 }
